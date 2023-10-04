cf91f8b2ac38ba43667088a19a95dd4c8bb8511a
j-adrian-lopez <adrj156@byui.edu>
2023-10-03 23:41:20 -0300

    Solved the Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,28 +1,7 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  if (!getLocalStorage("so-cart")) {
-    setLocalStorage("so-cart", []);
-  }
-  let cartProduct = getLocalStorage("so-cart");
-  cartProduct.push(product);
-  setLocalStorage("so-cart", cartProduct);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-const prodID = getParam('product');
+const prodID = getParam("product");
 productDetails(prodID);
 
-console.log(findProductById(prodID));
-
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// console.log(findProductById(prodID));
