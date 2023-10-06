a727377670dc267ce71b4344198eb39fffc64cee
j-adrian-lopez <adrj156@byui.edu>
2023-10-06 15:30:41 -0300

    Added id and started animation script

--- src/js/product.js
+++ src/js/product.js
@@ -5,3 +5,9 @@ const prodID = getParam("product");
 productDetails(prodID);
 
 // console.log(findProductById(prodID));
+
+function animateBackpack() {
+    let icon = document.getElementById("backpack");
+    
+  }
+  
