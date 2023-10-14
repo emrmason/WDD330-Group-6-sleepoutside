ebe5d16128bc1a52aa5fa7528b09e7a1136b16ac
j-adrian-lopez <adrj156@byui.edu>
2023-10-14 18:37:21 -0300

    Added icon shaking in products

--- src/js/product.js
+++ src/js/product.js
@@ -6,6 +6,11 @@ productDetails(prodID);
 
 // console.log(findProductById(prodID));
 
-function animateBackpack() {
+let button = document.getElementById("addToCart");
+button.addEventListener("click", () => {
   let icon = document.getElementById("backpack");
-}
+  icon.setAttribute("class", "shake");
+  setTimeout(() => {
+    icon.removeAttribute("class", "shake");
+  }, 350);
+});
