ea4524be615c6213ae8462df9fe46f775e13a040
j-adrian-lopez <adrj156@byui.edu>
2023-10-20 22:04:19 -0300

    Teamwork until step 4

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         product2: resolve(__dirname, "src/product_pages/index.html"),
         product3: resolve(__dirname, "src/product_pages/index.html"),
         product4: resolve(__dirname, "src/product_pages/index.html"),
+        product_list: resolve(__dirname, "src/product-list/index.html"),
       },
     },
   },
