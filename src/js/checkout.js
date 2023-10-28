3e0be4e4f9d79f81dbf6d08b9196825ee811415e
j-adrian-lopez <adrj156@byui.edu>
2023-10-28 18:05:14 -0300

    Finished checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,16 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+import checkoutProcess from "./checkoutProcess.mjs";
 
 loadHeaderFooter();
+
+checkoutProcess.init("so-cart", ".checkout-summary");
+
+document.querySelector("#zip").addEventListener("blur",
+
+checkoutProcess.calculateOrderTotal.bind(checkoutProcess)
+);
+
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+    e.preventDefault();
+    checkoutProcess.checkout(document.forms["checkout"]);
+});
