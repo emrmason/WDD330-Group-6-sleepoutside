07f6ec5a44e608decc7eec5bde92097eb613e166
emrmason <mas20021@byui.edu>
2023-11-10 17:01:06 -0800

    Team 8 through step 5

--- src/js/auth.mjs
+++ src/js/auth.mjs
@@ -14,18 +14,35 @@ export async function login(creds, redirect = "/"){
     }
 };
 
-function checkLogin(tokenKey){
-    if(tokenKey) {
-        jwt_decode();
-        const date = new Date();
-        
+export function checkLogin(){
+    const token = getLocalStorage(tokenKey);
+    const valid = isTokenValid(token);
+    if(!valid) {
+        localStorage.removeItem(tokenKey);
+        const location = window.location;
+        console.log(location);
+        window.location = `/login.index.html?redirect=${location.pathname}`;
     }
-    return
+    else{
+       return token; 
+    }
 }
 
 // is responsible for checking an existing token to make sure it is not expired. 
 // It should return a true or false, true if the token is still valid (unexpired), false if it is expired.
 
-function isTokenValid(){
-    return
+// How do we know if the token is expired? 
+
+function isTokenValid(token){
+    if(token) {
+        const decoded = jwt_decode(token);
+        const currentDate = new Date();
+        if(decoded.exp * 1000 < currentDate.getTime()) {
+            console.log("Token Expired");
+            return false;
+        } else {
+            console.log("Valid Token")
+            return true;
+        }
+    } else return false;
 }
--- src/js/orders.js
+++ src/js/orders.js
@@ -1,1 +1,7 @@
 // Placeholder for code
+// Need to redirect to login as soon as page loads
+import { loadHeaderFooter } from "./utils.mjs";
+import { checkLogin } from "./auth.mjs";
+
+loadHeaderFooter();
+checkLogin();
